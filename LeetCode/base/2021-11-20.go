08a1a363adee9d890b0fb1881513971e04d84586
xiong <xcy1124145271@126.com>
2021-11-21 12:01:58 +0800

    leetcode 学习

--- LeetCode/base/2021-11-20.go
+++ LeetCode/base/2021-11-20.go
@@ -5,6 +5,9 @@ import "fmt"
 func main() {
 	val := twoSum([]int{1, 2, 3, 4, 5}, 5)
 	fmt.Printf("%v", val)
+
+	val2 := reverse(2147483647)
+	fmt.Printf("%v", val2)
 }
 
 /*
@@ -26,6 +29,32 @@ func twoSum(nums []int, target int) []int {
 		} else {
 			mapResult[v] = i
 		}
+	}
+
+	return result
+}
+
+/*
+7. 整数反转
+给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
+如果反转后整数超过 32 位的有符号整数的范围[−2^31, 2^31− 1] ，就返回 0。
+假设环境不允许存储 64 位整数（有符号或无符号）。
+提示：-231 <= x <= 231 - 1
+*/
+func reverse(x int) int {
+	var result = 0
+	max := 1 << 31
+	if -max > x || x > max-1 {
+		return result
+	}
+
+	for x != 0 {
+		result = result*10 + x%10
+		x = x / 10
+	}
+
+	if -max >= result || result >= max-1 {
+		return 0
 	}
 
 	return result
